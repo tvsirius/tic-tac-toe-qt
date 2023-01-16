2e958f3c73214afbfdf5ba2782585adcc84c60a8
Denis Savitsky <densav@gmail.com>
2023-01-16 17:38:04 +0200

    interface layout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import PyQt6.QtCore
 
 from PyQt6.QtCore import Qt, QSize
 from PyQt6.QtWidgets import (QApplication,
@@ -10,7 +11,8 @@ from PyQt6.QtWidgets import (QApplication,
                              QLabel, QLCDNumber, QLineEdit, QMainWindow,
                              QProgressBar, QPushButton,
                              QRadioButton, QSlider, QSpinBox, QTimeEdit, QVBoxLayout, QHBoxLayout,
-                             QWidget, )
+                             QWidget, QDialog)
+import random
 
 stats = [0, 0, 0]
 game_count = 0
@@ -19,13 +21,20 @@ name = ''
 from board import Board
 
 
-class PlayWindow(QMainWindow):
+class PlayWindow(QDialog):
 
-    def __init__(self):
+    def __init__(self, main_win_ref, pl):
         super().__init__()
         self.setWindowTitle('TicTacToe - Play')
+        self.main_win_ref = main_win_ref
         self.board = Board()
-        self.ismove = False
+
+        self.humanmove = abs(pl - 2)
+        self.iswin = False
+        self.isdrawn = False
+
+        self.human_pl = pl
+        self.comp_pl = 1 if self.human_pl == 2 else 2
 
         layouts = [QHBoxLayout(), QHBoxLayout(), QHBoxLayout()]
 
@@ -56,19 +65,43 @@ class PlayWindow(QMainWindow):
             for y in range(3):
                 self.buttons[x][y].clicked.connect(self.field_clicked_wrap(y, x))
 
-        widget = QWidget()
-        widget.setLayout(fin_layout)
+        self.setLayout(fin_layout)
 
-        self.setCentralWidget(widget)
+        if not self.humanmove:
+            self.computer_move()
+
+    def show_board(self):
+        pass
+
+    def gameover_check(self):
+        self.iswin = self.board.checkwin_all_players(2)
+        self.isdrawn = self.board.full()
+
+        if self.iswin or self.isdrawn:
+            self.finish()
+
+    def finish(self):
+        pass
+
+
+    def computer_move(self):
+        comp_vars = self.board.computer_move(self.comp_pl)
+
+        x, y = random.choice(comp_vars)
+        self.board[x][y] = self.comp_pl
+
+        self.gameover_check()
+
+        self.humanmove=True
+
 
     # @xy_decor_wrap()
     def field_clicked_wrap(self, x, y):
-        x_in = x
-        y_in = y
-
         def wrap():
-            self.label1.setText(f"x={x} , y={y}")
-            pass
+            if self.humanmove:
+                self.label1.setText(f"x={x} , y={y}")
+                self.board.try_move(x,y)
+
 
         return wrap
 
@@ -80,24 +113,24 @@ class MainWindow(QMainWindow):
         self.isplay = False
 
         layoutH = QHBoxLayout()
-        buttons = [QPushButton(), QPushButton(), QPushButton()]
+        self.buttons = [QPushButton(), QPushButton(), QPushButton()]
 
-        for button in buttons:
+        for button in self.buttons:
             layoutH.addWidget(button)
 
-        buttons[0].setText("Human first")
-        buttons[0].clicked.connect(self.newgame_human)
-        buttons[1].setText("Computer first")
-        buttons[1].clicked.connect(self.newgame_comp)
-        buttons[2].setText("exit")
-        buttons[2].clicked.connect(self.press_exit)
+        self.buttons[0].setText("Human first")
+        self.buttons[0].clicked.connect(self.newgame_human)
+        self.buttons[1].setText("Computer first")
+        self.buttons[1].clicked.connect(self.newgame_comp)
+        self.buttons[2].setText("exit")
+        self.buttons[2].clicked.connect(self.press_exit)
 
         button_widget = QWidget()
         button_widget.setLayout(layoutH)
 
         layoutV = QVBoxLayout()
         self.labels = [QLabel() for i in range(5)]
-        self.labels[0].setText("Tic tac toe game!")
+        self.labels[0].setText(f"{user_name} welcome to Tic tac toe!")
         self.labels[0].setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.labels[0].setMinimumHeight(15)
         self.labels[4].setText("New game ?")
@@ -116,18 +149,22 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(widget)
 
     def show_score(self):
-        self.labels[1].setText(f'You wins: {stats[0]}')
+        self.labels[1].setText(f'{name} wins: {stats[0]}')
         self.labels[2].setText(f'Computer wins: {stats[1]}')
         self.labels[3].setText(f'Draws: {stats[2]}')
 
-    def newgame_human(self):
-        self.play_window = PlayWindow(self, human)
-        self.play_window.show()
+    def newgame(self, pl):
+        self.play_window = PlayWindow(self, pl)
         self.isplay = True
 
-    def newgame_comp(self):
-        self.play_window = PlayWindow()
+        self.play_window.setModal(True)
         self.play_window.show()
+
+    def newgame_human(self):
+        self.newgame(1)
+
+    def newgame_comp(self):
+        self.newgame(2)
 
     def press_exit(self):
         self.close()
@@ -163,7 +200,7 @@ class WelcomeWindow(QMainWindow):
     def enter(self):
         if len(self.textfield.text()) > 0:
             global name
-            name=self.textfield.text()
+            name = self.textfield.text()
             self.main_window = MainWindow(name)
             self.main_window.show()
             self.close()
